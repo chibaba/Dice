ec09e0d4a404d5c214edb2a4ad5e26541a5d2a7f
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-03-23 03:06:45 +0100

    adding gesture detection with flutter button

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueAccent,
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
@@ -18,14 +18,32 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-       child: Image(
-        image: AssetImage('images/dice1.png'),
-        ),
-    ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+         child: Padding(
+           padding: const EdgeInsets.all(16.0),
+           child: FlatButton(
+             child: Image.asset(
+              'images/dice1.png'
+              ),
+           ),
+         ),
+      ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                child: Image.asset(
+                  'images/dice1.png'
+                ),
+              ),
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 }
